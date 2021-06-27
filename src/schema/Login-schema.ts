a83696abf815bdd20ea98a6a734b9c4d60b3b2d8
rafaelbercam <rbercam.medeiros@gmail.com>
2021-06-27 15:29:46 -0300

    add login schema validation

---
+++ src/schema/Login-schema.ts
@@ -0,0 +1,12 @@
+import Joi = require('joi');
+
+const loginSchema = Joi.object({
+    message: Joi.string().required(),
+    authorization: [Joi.string(),Joi.number()]
+})
+
+const loginFailSchema = Joi.object({
+    message: Joi.string().required()
+})
+
+module.exports = { loginFailSchema, loginSchema};
--- src/test/Login-test.ts
+++ src/test/Login-test.ts
@@ -1,8 +1,10 @@
+import Joi = require('joi');
 import { expect } from "chai";
 import loginFactory from "../factory/Login-factory";
 import UserFactory from "../factory/User-factory";
 import { postLogin } from "../services/Login-service";
 import { postUser } from "../services/Users-service";
+const schema = require('../schema/Login-schema')
 
 require ('dotenv').config({ path: '../../.env' })
 
@@ -19,13 +21,15 @@ describe('Login test request', async ()=>{
     it('Login Success', async ()=>{       
         response = await postLogin(user);
         expect(response.statusCode).to.eq(200);
-        expect(response.body.message).to.eq('Login realizado com sucesso');   
+        expect(response.body.message).to.eq('Login realizado com sucesso');  
+        Joi.assert(response.body, schema.loginSchema)
     })
 
     it('Login test Fail - wrong credentials', async () => {
         response = await postLogin(loginFactory.loginFail);
         expect(response.statusCode).to.eq(401);
         expect(response.body.message).to.eq('Email e/ou senha inválidos');
+        Joi.assert(response.body, schema.loginFailSchema)
     })
 
     it('Login test Fail - email required', async () => {
